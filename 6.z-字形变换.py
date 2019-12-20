ce9175aad663ba9451062c2a0414876879449246
zzt <zzt@zztdeiMac.local>
2019-12-20 10:06:50 +0800

    soluton 6

---
+++ 6.z-字形变换.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=6 lang=python3
+#
+# [6] Z 字形变换
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2:
+            return s
+        
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1:
+                flag = -flag
+            i += flag
+            
+        return "".join(res)
+        
+# @lc code=end
+

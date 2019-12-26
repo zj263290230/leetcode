1191b28d46a0cb146569efd8c9d0b518d4259adf
zzt <zzt@zztdeiMac.local>
2019-12-26 14:34:08 +0800

    solution 36

---
+++ 36.有效的数独.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=36 lang=python3
+#
+# [36] 有效的数独
+#
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [[x for x in y if x != '.'] for y in board]
+        col = [[x for x in y if x != '.'] for y in zip(*board)]
+        pal = [[board[i+m][j+n] for m in range(3) for n in range(3) if board[i+m][j+n] != '.'] for i in (0, 3, 6) for j in (0, 3, 6)]
+        return all(len(set(x)) == len(x) for x in (*row, *col, *pal))
+        
+# @lc code=end
+
